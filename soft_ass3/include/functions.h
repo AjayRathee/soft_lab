e8f9597d6ce53608ca83f2a09191807453e434e4
Ajay Rathee <replymeat@gmail.com>
2015-01-31 23:21:52 +0530

    Split files

---
+++ soft_ass3/include/functions.h
@@ -0,0 +1,14 @@
+/*
+ * functions.h
+ *
+ *  Created on: 31-Jan-2015
+ *      Author: ajay
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+int fun(int len, char*buffer,FILE *fp_w);
+void operation();
+
+#endif /* FUNCTIONS_H_ */
